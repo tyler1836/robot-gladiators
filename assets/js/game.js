c4561ea811be9cdf44f40462de30a681e8c51051
unknown <tyler.r.roll@gmail.com>
2021-06-23 02:06:20 -0600

    for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,13 +6,12 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth, playerMoney);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose. ");
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -57,5 +56,6 @@ else {
   window.alert("You need to choose a valid option. Try again!");
  }
 }
-
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+  fight(enemyNames[i]);
+   }
